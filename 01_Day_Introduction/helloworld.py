2cc74663e8305b33b04746b7a9b02343ba7939e3
Matthias Annen <matthias.annen@hotmail.com>
2022-07-24 15:35:29 +0200

    euclidian distance added

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,35 +1,41 @@
 # Introduction
 # Day 1 - 30DaysOfPython Challenge
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
-print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+# print(2 + 3)   # addition(+)
+# print(3 - 1)   # subtraction(-)
+# print(2 * 3)   # multiplication(*)
+# print(3 / 2)   # division(/)
+# print(3 ** 2)  # exponential(**)
+# print(3 % 2)   # modulus(%)
+# print(3 // 2)  # Floor division operator(//)
 
-# Checking data types
+# # Checking data types
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+# print(type(10))                  # Int
+# print(type(3.14))                # Float
+# print(type(1 + 3j))              # Complex
+# print(type('Asabeneh'))          # String
+# print(type([1, 2, 3]))           # List
+# print(type({'name':'Asabeneh'})) # Dictionary
+# print(type({9.8, 3.14, 2.7}))    # Set
+# print(type((9.8, 3.14, 2.7)))    # Tuple
 
-phone_book = {"first_name": "Matthias", "last_name": "Annen"}
+# phone_book = {"first_name": "Matthias", "last_name": "Annen"}
 
-print(phone_book["first_name"])
-print("--------------")
-print(type(2))
-print(type(3.2))
-print(type("text"))
-print(type(True))
-print(type([1, 2]))
-print(type((1, 2)))
-print(type({1, 2, 5}))
-print(type({"phone_number": "23232"}))
+# print(phone_book["first_name"])
+# print("--------------")
+# print(type(2))
+# print(type(3.2))
+# print(type("text"))
+# print(type(True))
+# print(type([1, 2]))
+# print(type((1, 2)))
+# print(type({1, 2, 5}))
+# print(type({"phone_number": "23232"}))
 
+tuple1 = (2, 3)
+tuple2 = (10, 8)
+print(f"tuple1[0]: {tuple1[0]}")
+print(f"tuple2[0]: {tuple2[0]}")
+
+print((tuple1[0]-tuple2[0])**2 + (tuple1[1]-tuple2[1])**2)

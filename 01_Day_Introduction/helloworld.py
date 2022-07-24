1388c1db011fb72040bcfed73dc01c02a2811c02
Matthias Annen <matthias.annen@hotmail.com>
2022-07-24 15:15:08 +0200

    own examples

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -19,3 +19,17 @@ print(type([1, 2, 3]))           # List
 print(type({'name':'Asabeneh'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
+
+phone_book = {"first_name": "Matthias", "last_name": "Annen"}
+
+print(phone_book["first_name"])
+print("--------------")
+print(type(2))
+print(type(3.2))
+print(type("text"))
+print(type(True))
+print(type([1, 2]))
+print(type((1, 2)))
+print(type({1, 2, 5}))
+print(type({"phone_number": "23232"}))
+

c2d9dd79c2e66bd9ccbd5593f0abe95c70f859b4
Matthias Annen <matthias.annen@hotmail.com>
2022-07-24 18:19:17 +0200

    add exercises of day 3

---
+++ 03_Day_Operators/Exercises_day_3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 16:34:47 2022
+
+@author: MatthiasAnnen
+"""
+
+# age = 38
+# height = 1.85
+# complex_number = 3 + 1j
+
+base = int(input("enter the base: "))
+height = int(input("enter the height"))
+print("The area of the triangle is: ", base*height/2)
+
